d3927c443039db21f1514e20893a63ad7eecd450
xJakubix <lieskejakub@gmail.com>
2023-03-16 11:34:58 +0100

    Tailwind install

--- login-app/src/components/LoginPage.js
+++ login-app/src/components/LoginPage.js
@@ -2,8 +2,17 @@ import "bootstrap/dist/css/bootstrap.css";
 
 export default function LoginPage() {
   return (
-    <div>
-      LoginPage
-    </div>
+        <div>
+             <h1 className="text-5xl font-bold underline">
+                Hello world!
+            </h1>
+            <form method="post" action="auth.php">
+                <input className="text-blue-600" type="text" name="login" placeholder="login"/>
+                <br/>
+                <input type="password" name="password" placeholder="hasło"/>
+                <br/>
+                <input type="submit"/>
+            </form>
+        </div>
   );
 }
